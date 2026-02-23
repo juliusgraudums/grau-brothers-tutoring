af48f8d0f0e739237d8f62bfdb1f7241585f2e97
juliusgraudums <julius.graudums@gmail.com>
2026-02-23 23:18:51 +0100

    Adjust landing sections and hero CTAs to user feedback

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 import Laxhjalp from "./pages/Laxhjalp";
 import Tjanster from "./pages/Tjanster";
+import Coaching from "./pages/Coaching";
+import Ovrigt from "./pages/Ovrigt";
+import Kontakt from "./pages/Kontakt";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,8 +21,19 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/laxhjalp" element={<Laxhjalp />} />
-          <Route path="/tjanster" element={<Tjanster />} />
+
+          <Route path="/tjanster">
+            <Route index element={<Tjanster />} />
+            <Route path="laxhjalp" element={<Laxhjalp />} />
+            <Route path="coaching" element={<Coaching />} />
+            <Route path="ovrigt" element={<Ovrigt />} />
+          </Route>
+
+          <Route path="/kontakt" element={<Kontakt />} />
+
+          <Route path="/laxhjalp" element={<Navigate to="/tjanster/laxhjalp" replace />} />
+          <Route path="/coaching" element={<Navigate to="/tjanster/coaching" replace />} />
+
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
